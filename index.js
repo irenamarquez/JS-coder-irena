b75c22a2aea8d8cb04c149f3dd69add45d7d5a92
irenamarquez <irena.pmh@gmail.com>
2022-07-22 00:40:09 -0300

    cambios
    cambios

--- index.js
+++ index.js
@@ -1,4 +1,77 @@
-{  
+const productos = [
+  { nombre:"jersey", precio: 60 };
+  { nombre: "bib", precio: 45 };
+  { nombre: "media", precio: 15 };
+  { nombre: "cap", precio: 20 };
+]
+
+let carrito = []
+
+let seleccion = prompt ("Hola! Deseas comprar algun producto ALFHO, si o no");
+
+while(seleccion != "si" && seleccion != "no"){
+  alert("por favor ingresa: si o no");
+  seleccion = prompt("Deseas comprar algo, si o no");
+}
+
+if(seleccion == "si"){
+  alert("continuar a lista de productos");
+  let todoslosProductos = productos.map(
+    (producto) => producto.nombre + "" + producto.precio + "$");
+    alert(todoslosProductos.join("-"));
+}
+
+else if (seleccion == "no"){
+  alert("Gracias, hasta pronto!");
+}
+
+while(seleccion != "no"){
+  let producto = prompt("agregar a carrito");
+  let precio = 0;
+
+  if(producto == "jersey" || producto == "bib" || producto == "media" || producto == "cap"){
+    switch(producto) {
+      case "jersey":
+        precio = 60;
+        break;
+      case "bib":
+        precio = 45;
+        break;
+      case "media":
+        precio = 15;
+        break;
+      case "cap":
+        precio = 20;
+        break;
+    }
+    let unidades = parseInt(prompt("cantidad de productos"));
+
+    carrito.push({producto, unidades, precio});
+
+    console.log(carrito);
+  }
+  else{
+    alert("producto no disponible");
+  }
+
+  seleccion = prompt("desea seguir comprando?");
+
+  while(seleccion == "no"){
+    alert("gracias por su compra! hasta pronto");
+    carrito.forEach((carritoFinal) => {
+      console.log(`producto: ${carritoFinal.producto}, unidades: ${carrito.unidades},
+      total a pagar por producto ${carritoFinal.unidades * carritoFinal.precio} `)
+    })
+    break;
+  }
+
+}
+const total = carrito.reduce((acc, el) => acc + el.precio * el.unidades, 0);
+
+console.log(`el total a pagar por su compra es: ${total}`)
+
+
+/*{
   function Users(nombre, apellido, edad, email, password)
     this.nombre = nombre;
     this.apellido = apellido;
@@ -19,7 +92,7 @@
   this.codigo = codigo;
   
   
-  let user = prompt("Completar datos");
+  let user1 = prompt("Completar datos");
 
   let nombre = prompt("Ingrese nombre del usuario");
   let apellido = prompt("Ingrese apellido del usuario");
@@ -27,7 +100,7 @@
   let email = prompt("Ingrese email del usuario");
   let password = prompt("Ingrese password del usuario");
   
-  let carritoCompra = prompt(agregar al carrito)
+  let carritoCompra = prompt(agregar al carrito);
   
   if(user.userIsCompleted()){
       users.push(user);
@@ -36,22 +109,22 @@
       alert(`Usuario NO registrado.`);       
   }
     
-let user = "irena";
-if(user === "irena"){
+let user1 = "irena";
+if(user1 === "irena"){
   console.log("saludo");
 }else{
   console.log("no saludo");
 }
-if(user === "irena" || user === "irena"){
+if(user1 === "irena" || user1 === "irena"){
     alert("hola irena :)");
   }else{
     alert("Quién eres?")
   }
 
-  let user = prompt("Ingrese un usuario").toLowerCase();
-  while(user = "irena"){
+  let user1 = prompt("Ingrese un usuario").toLowerCase();
+  while(user1 = "irena"){
     alert("Usuario incorrecto");
-    user = prompt("Ingrese un usuario");
+    user1 = prompt("Ingrese un usuario");
   }
 
-}
+}*/
